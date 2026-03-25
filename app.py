3a4927c9c8857ede3099d054831716cf5cc59825
Harsh Thakar <hthakar812@gmail.com>
2026-03-25 15:23:23 +0530

    Implemented TF-IDF from scratch

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from utils.text_extractor import extract_text
 from utils.text_preprocessor import preprocess_text
+from utils.tfidf_vectorizer import compute_tf, compute_idf, compute_tfidf
 
 file_path = "data/sample_resume.txt"
 
 # Step 1: Extract text
 text = extract_text(file_path)
 
-print("----- Original Text -----\n")
-print(text)
-
 # Step 2: Clean text
 clean_text = preprocess_text(text)
 
-print("\n----- Cleaned Text -----\n")
-print(clean_text)
+# Step 3: Prepare documents (for now only 1)
+documents = [clean_text]
+
+# Step 4: Compute TF
+tf = compute_tf(clean_text)
+
+# Step 5: Compute IDF
+idf = compute_idf(documents)
+
+# Step 6: Compute TF-IDF
+tfidf = compute_tfidf(tf, idf)
+
+print("----- TF-IDF Scores -----\n")
+for word, score in tfidf.items():
+    print(f"{word}: {score:.4f}")
---
+++ utils/tfidf_vectorizer.py
@@ -0,0 +1,39 @@
+import math
+from collections import Counter
+
+def compute_tf(text):
+    words = text.split()
+    word_count = len(words)
+
+    tf_dict = {}
+
+    for word in words:
+        tf_dict[word] = tf_dict.get(word, 0) + 1
+
+    for word in tf_dict:
+        tf_dict[word] = tf_dict[word] / word_count
+
+    return tf_dict
+
+
+def compute_idf(documents):
+    import math
+    N = len(documents)
+    idf_dict = {}
+
+    all_words = set(word for doc in documents for word in doc.split())
+
+    for word in all_words:
+        count = sum(1 for doc in documents if word in doc.split())
+        idf_dict[word] = math.log(N / (1 + count))
+
+    return idf_dict
+
+
+def compute_tfidf(tf, idf):
+    tfidf = {}
+
+    for word in tf:
+        tfidf[word] = tf[word] * idf.get(word, 0)
+
+    return tfidf
