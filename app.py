f5e5d3864a822018ba03a446d0f14a859e706bcb
Harsh Thakar <hthakar812@gmail.com>
2026-03-25 15:01:18 +0530

    Added text preprocessing

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from utils.text_extractor import extract_text
+from utils.text_preprocessor import preprocess_text
 
-# Path to your resume file
 file_path = "data/sample_resume.txt"
 
-# Extract text
+# Step 1: Extract text
 text = extract_text(file_path)
 
-# Print output
-print("----- Resume Content -----\n")
+print("----- Original Text -----\n")
 print(text)
+
+# Step 2: Clean text
+clean_text = preprocess_text(text)
+
+print("\n----- Cleaned Text -----\n")
+print(clean_text)
---
+++ utils/text_preprocessor.py
@@ -0,0 +1,24 @@
+import re
+
+# Basic list of stopwords (common useless words)
+STOPWORDS = [
+    "and", "or", "the", "is", "in", "at", "of", "a", "to", "for", "on"
+]
+
+def preprocess_text(text):
+    # 1. Convert to lowercase
+    text = text.lower()
+
+    # 2. Remove special characters and numbers
+    text = re.sub(r'[^a-z\s]', '', text)
+
+    # 3. Tokenization (split into words)
+    words = text.split()
+
+    # 4. Remove stopwords
+    filtered_words = [word for word in words if word not in STOPWORDS]
+
+    # 5. Join back to string
+    clean_text = " ".join(filtered_words)
+
+    return clean_text
